e22d8dc61329e933a5c4ee8369f6717819232c26
Frynnn-69 <firyanfatihh@gmail.com>
2025-05-30 19:53:25 +0700

    feat: Integrate AdminRole middleware for admin access control

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+    $middleware->alias([
+        'admin' => \App\Http\Middleware\AdminRole::class,
+    ]);
+    
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
